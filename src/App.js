5c4c04d42ac78105ae9daad2ef14b382502feeef
alvyynm <alvyynm@gmail.com>
2022-10-30 10:54:48 +0300

    import and display Hero component in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.scss";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
+      <Hero />
       <p>Team Helsinki Hello World</p>
       <p>Sass works!</p>
     </div>

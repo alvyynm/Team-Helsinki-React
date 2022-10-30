221c0d67583098a0445ae91ce715fff7e07d8090
alvyynm <alvyynm@gmail.com>
2022-10-30 10:53:43 +0300

    create /components with Hero.jsx and Hero.scss with initial content to test

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hero.scss";
+
+function Hero() {
+  return (
+    <section>
+      <p className="hero">Hero component</p>
+    </section>
+  );
+}
+
+export default Hero;

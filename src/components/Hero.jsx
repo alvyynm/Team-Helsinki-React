b771c54fbc76ab3f5c6e381e1319066f0b117502
alvyynm <alvyynm@gmail.com>
2022-10-31 16:24:06 +0300

    add jsx content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,25 @@ import "./Hero.scss";
 
 function Hero() {
   return (
-    <section>
+    <section className="hero" aria-labelledby="heroSection">
+      <div className="hero__wrapper">
+        <header className="hero__title">
+          <h1 id="heroSection">
+            Welcome to
+            <span className="hero__title--green">your safe space</span>
+            in the tech ecosystem
+          </h1>
+        </header>
+        <div className="hero__content">
+          <p className="hero__content__text">
+            Finding a job shouldn’t be a job. Tell us what you’re looking for
+            and we’ll get it for you. Explore more than 5600+ jobs closeby!
+          </p>
+          <button className="hero__content__btn" type="button">
+            Get Started
+          </button>
+        </div>
+      </div>
       <p className="hero">Hero component</p>
     </section>
   );
